4735ab524ae116da0cbfa7d067a4e5eb5e39a7a9
Morten Jessen <mortenjessen5d@gmail.com>
2022-02-03 22:39:29 +0100

    Setup project

---
+++ src/pages/404.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react"
+import { Link } from "gatsby"
+
+const NotFoundPage: FC = () => {
+  return (
+    <main>
+      <title>Not found</title>
+      <h1>Page not found</h1>
+      <p>Sorry{" "}
+        <span role="img" aria-label="Pensive emoji">
+          😔
+        </span>{" "}
+        we couldn't find what you were looking for.
+        <br />
+        <Link to="/">Go home</Link>.
+      </p>
+    </main>
+  )
+}
+
+export default NotFoundPage
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react"
+
+const IndexPage: FC = () => {
+  return (
+    <main>
+      <title>Hej.</title>
+      <p>Hej.</p>
+    </main>
+  )
+}
+
+export default IndexPage
