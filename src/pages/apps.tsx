df7c72acb622b65e2d829a7d3994f33f95c56387
Morten Jessen <mortenjessen5d@gmail.com>
2022-02-13 09:55:02 +0100

    Add apps page

---
+++ src/pages/apps.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { Link, Page } from "../components";
+
+const AppsPage: FC = () => {
+  return (
+    <Page headline="Apps">
+      <p>There's nothing here :-(</p>
+    </Page>
+  )
+}
+
+export default AppsPage
